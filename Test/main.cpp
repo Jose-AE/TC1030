74a70e47a3a6afc2b71c2433142358f7235e2d8d
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-06-08 21:26:42 -0600

    virtual example

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include "Classes/user.h"
 
+// pure virtual
+class person {
+  public:
+   virtual int getNum() = 0;
+};
+
+class bob : public person {
+
+   int getNum() override {}
+};
+
+class entity {
+  public:
+   virtual int getNum() {}
+};
+
+class enemy : public entity {
+   int getNum() override {}
+};
+
 int main() {
    User user("JohnDoe", "pa$$w0rd");
    user.displayUserDetails();
