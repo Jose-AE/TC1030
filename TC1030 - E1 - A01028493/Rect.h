6eecc177eeb257935d8e8129642faf974497fa7c
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2023-06-09 12:59:21 -0600

    Created E1

---
+++ TC1030 - E1 - A01028493/Rect.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Polar;
+
+class Rect {
+  private:
+   float x;
+   float y;
+
+  public:
+   Rect();
+   Rect(float, float);
+   Rect operator+(const Rect&) const;
+   Rect operator+(const Polar&) const;
+   Rect operator-(const Rect&) const;
+   Rect operator-(const Polar&) const;
+   float getX() const;
+   float getY() const;
+   void print() const;
+};
